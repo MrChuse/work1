c439fa34b867e5e78294bdbaa5a72b730ab1b2a0
MrChuse <43815531+MrChuse@users.noreply.github.com>
2020-09-27 04:18:19 +0300

    Added item_category.sql

---
+++ item_category.sql
@@ -0,0 +1,6 @@
+select
+Item, Category
+from ItemsAndCategories
+order by Item;
+--not really sure what to do with this many to many relationship
+--it's that simple if all Items are duplicated when it has more than one category
